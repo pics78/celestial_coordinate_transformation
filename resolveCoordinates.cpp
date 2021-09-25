61045a5c3c815bfbd946842eb1a370653396bd84
pics78 <mcmc.k0206@gmail.com>
2021-09-25 22:54:58 +0900

    Lightly simplified

--- resolveCoordinates.cpp
+++ resolveCoordinates.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<regex>
-#include <unistd.h>
+#include<unistd.h>
 using namespace std;
 
 #include "toolsForObs.hpp"
+
+#define OPTION_EQU_TO_HOR 'h' // Equatorial coordinates -> Horizontal coordinates
+#define OPTION_HOR_TO_EQU 'e' // Horizontal coordinates -> Equatorial coordinates
 
 #define ERROR_MESSAGE1 "-Usage : %s [-h] or [-e] arg1 arg2\n"
 #define ERROR_MESSAGE2 "-input mistake: 0 <= arg1 < 360\n"
@@ -13,23 +16,11 @@ using namespace std;
 #define CONF_FILE_NAME "local_info.conf"
 
 int main(int argc, char* argv[]) {
-	// 0: Equatorial coordinates -> Horizontal coordinates (option:h)
-	// 1: Horizontal coordinates -> Equatorial coordinates (option:e)
-	int mode;
-
 	int option = getopt(argc, argv, "he:");
-	switch (option) {
-		case 'h':
-			mode = 0;
-			break;
-		case 'e':
-			mode = 1;
-			break;
-		default:
-			printf(ERROR_MESSAGE1, argv[0]);
-			return 1;
+	if (option != OPTION_EQU_TO_HOR && option != OPTION_HOR_TO_EQU) {
+		printf(ERROR_MESSAGE1, argv[0]);
+		return 1;
 	}
-
 	if (argc < 4) {
 		printf(ERROR_MESSAGE1, argv[0]);
 		return 1;
@@ -46,13 +37,13 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	double latitude = -1;
+	double latitude  = -1;
 	double longitude = -1;
-	ifstream reading_file;
-	reading_file.open(CONF_FILE_NAME, ios::in);
+	ifstream ifs;
 	string buf;
 	smatch results;
-    while(getline(reading_file, buf)){
+	ifs.open(CONF_FILE_NAME, ios::in);
+	while (getline(ifs, buf)) {
 		if (regex_search(buf, results, regex("^ *Latitude *= *([\\d\\.]+) *$"))) {
 			latitude = stof(results[1].str());
 		} else if (regex_search(buf, results, regex("^ *Longitude *= *([\\d\\.]+) *$"))) {
@@ -68,10 +59,10 @@ int main(int argc, char* argv[]) {
 	double inpc[2] = {x, y};
 	double outc[2] = {0, 0};
 
-	if (mode == 0) {
+	if (option == OPTION_EQU_TO_HOR) {
 		ConvEquToHor(now, inpc, outc);
 		printf("A=%.3lf, h=%.3lf\n", outc[0], outc[1]);
-	} else if (mode == 1) {
+	} else if (option == OPTION_HOR_TO_EQU) {
 		ConvHorToEqu(now, inpc, outc);
 		printf("RA=%.3lf, Dec=%.3lf\n", outc[0], outc[1]);
 	}
--- toolsForObs.hpp
+++ toolsForObs.hpp
@@ -1,6 +1,6 @@
-#include<fstream>
 #include<math.h>
 #include<time.h>
+#include<fstream>
 
 const double _DegToRad = M_PI/180;
 const double _RadToDeg = 180/M_PI;
@@ -74,50 +74,49 @@ class NOWs {
 
 		// UTC [h]
 		double CoordinatedUniversalTime() {
-			double UTC_Box = nJST - 9.0;
-			if(UTC_Box < 0) UTC_Box += 24.0;
-			return UTC_Box;
+			double UTCbox = nJST - 9.0;
+			return UTCbox >= 0 ? UTCbox : UTCbox + 24.0;
 		}
 
 		// ユリウス日 [day]
 		double JulianDay(std::string which) {
-			int mYear = Year, mMonth = Month;
-			double JD_makingBox = 0;
-			if(Month <= 2) {
+			int mYear  = Year;
+			int mMonth = Month;
+			double JDbox = 0;
+			if (Month <= 2) {
 				mYear -= 1;
 				mMonth += 12;
 			}
-			JD_makingBox += (int)(365.25*mYear);
-			JD_makingBox += (int)(mYear/400.0) - (int)(mYear/100.0);
-			JD_makingBox += (int)(30.59*(mMonth-2.0)) + Day + 1721088.5;
-			JD_makingBox += Hour/24.0 + Minute/1440.0 + Second/86400.0;
+			JDbox += (int)(365.25*mYear);
+			JDbox += (int)(mYear/400.0) - (int)(mYear/100.0);
+			JDbox += (int)(30.59*(mMonth-2.0)) + Day + 1721088.5;
+			JDbox += Hour/24.0 + Minute/1440.0 + Second/86400.0;
 
-			if(which == "UTC") JD_makingBox -= 9.0 / 24.0;
-			return JD_makingBox;
+			return which != "UTC" ? JDbox : JDbox - 9.0/24.0;
 		}
 
 		// グリニッジ標準時 [h]
 		double GreenwichSiderealTime() {
 			double D = nUMJD - 40000.0;
 			double temp = 0.67239 + (1.00273781 * D);
-			double GST_Box = 24.0 * (temp - (int)temp);
-			return GST_Box;
+			return 24.0 * (temp - (int)temp);
 		}
 
 		// 地方恒星時 [h]
 		double LocalSiderealTime() {
 			double _longitude = Longitude/15.0;
-			double LST_Box = nGST + _longitude;
-			if(LST_Box < 0) LST_Box += 24.0;
-			else if(LST_Box >= 24) LST_Box -= 24.0;
-			return LST_Box;
+			double LSTbox = nGST + _longitude;
+			return
+				LSTbox < 0   ? LSTbox + 24.0 :
+				LSTbox >= 24 ? LSTbox - 24.0 :
+				LSTbox;
 		}
 };
 
 // 時角 [degree]
 double HourAngle(NOWs thisTime, double RA) {
 	double H = thisTime.LST()*15 - RA;
-	return H >= 0 ? H : H + 360;
+	return H >= 0 ? H : H + 360.0;
 }
 
 // 赤道座標 Equ(RA,Dec) -> 地平座標 Hor(A,h)
